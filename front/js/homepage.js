741c26a5ecef88d0f44b114be9c2ad01120d4c9d
Kevin Teillier <kt.teillier@gmail.com>
2022-08-23 19:00:59 +0200

    a href modif, id response on each page product

--- front/js/homepage.js
+++ front/js/homepage.js
@@ -1,16 +1,24 @@
+
 fetch('http://localhost:3000/api/products')
     .then(data => data.json())
     .then(jsonItemList => {
         for(let jsonItem of jsonItemList){
             let item = new Items(jsonItem);
-            document.getElementById('items').innerHTML += `<a href="./product.html?id=42">
+            document.getElementById('items').innerHTML += `<a href="./product.html?id=${item._id}">
                                                             <article>
                                                                 <img src="${item.imageUrl}" alt="${item.altTxt}, ${item.name}">
                                                                 <h3 class="productName">${item.name}</h3>
                                                                 <p class="productDescription">${item.description}</p>
                                                             </article>
-                                                        </a>`;
+                                                        </a>
+                                                        `;                            
         }
     });
 
     
+  
+    
+
+
+    
+
--- front/js/item.js
+++ front/js/item.js
@@ -3,3 +3,9 @@ class Items{
         jsonItem && Object.assign(this, jsonItem);
     }
 }
+
+
+const urlData = window.location.search;
+
+const search_params = new URLSearchParams(urlData);
+const url_id = search_params.get("id");
