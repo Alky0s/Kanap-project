e7106b2073c7bcc174d98da708fe5d753bbf34fc
Kevin Teillier <kt.teillier@gmail.com>
2022-09-05 11:00:38 +0200

    add different items and right quantity values in localStorage

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,50 +1,81 @@
+// const productId = getProductId();
 
+// const productColor = getProductColor();
 
-// // Local storage
-// function CartNumbers (product) {
-//     let productNumbers = localStorage.getItem('CartNumbers'.value);
+// const productQuantity = getProductQuantity();
+// console.log(productQuantity);
 
-//     productNumbers = parseInt(productNumbers);
+// fetch('http://localhost:3000/api/products/' + productId)
+//     .then(data => data.json())
+//     .then(jsonProduct => {
+//         // Collecting title, price and description
+//         document.getElementById('title').textContent = jsonProduct.name;
+//         document.getElementById('price').textContent = jsonProduct.price;
+//         document.getElementById('description').textContent = jsonProduct.description;
+        
+        
+//         // For selecting color 
+//         const colorsSelect = jsonProduct.colors;
+        
+//         for (const color of colorsSelect) {
+//             document.getElementById('colors').innerHTML += `<option value="${color.toLowerCase()}">${color}</option>`;                                     
+//         }
+        
+//         // Collecting images from API
+//         const productImageUrl = jsonProduct.imageUrl;
+//         const productImageAlt = jsonProduct.altTxt;
+//         document.querySelector('.item__img').innerHTML = `<img src="${productImageUrl}" alt="${productImageAlt}">`;
+ 
+//         // Adding products to cart + eventListener action on click
+//         const addToCart = document.getElementById("addToCart");
+//         addToCart.addEventListener('click', (event) => { 
+//             cartNumbers();
+//             event.preventDefault();
+        
+//         // Create a function to add products in localstorage
 
-//     if (productNumbers) {
-//         localStorage.setItem('CartNumbers', productNumbers + 1);
-//     } else {
-//         localStorage.setItem('CartNumbers', 1);
-//     }
-//     setItems(product);
-// }  
-// function setItems(product) {
-//     // console.log("Inside of setItems");
-//     // console.log("Look at", product);
-    
-
-//     let cartItems = localStorage.getItem('productsInCart');
-//     cartItems = JSON.parse(cartItems);
-//     console.log("My cart items are", cartItems);
-
-//     if(cartItems != null) {
-//         if (cartItems[product.id] == undefined) {
-//             cartItems = {
-//                 ...cartItems,
-//                 [product.id]: product
+//         function cartNumbers(cartNumbers) {
+//             let productNumbers = localStorage.getItem('cartNumbers');
+//             productNumbers = parseInt(productNumbers);
+            
+//             if (productNumbers) {
+//                 localStorage.setItem('cartNumbers', productNumbers + parseInt(productQuantity));
+//             } else {
+//                 localStorage.setItem('cartNumbers', parseInt(productQuantity));
 //             }
 //         }
-//         cartItems[product.id].quantity += 1;
-//     } else {
-//         product.quantity = 1;
-//         cartItems = {
-//             [product.id]: product
-//         }
-//     }
-    
-//     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 
-//     // if (productsInCart) {
-//     //     localStorage.setItem('cartItems', productsInCart + 1);
-//     // } else {
-//     //     localStorage.setItem('cartItems', 1);
-//     // }
+//         // Array creation for Local storage
+//         const productArray = [
+//             {
+//                 id: productId,
+//                 quantity : parseInt(productQuantity),
+//                 color : productColor
+//             }
+//         ];
+//         console.log(productArray);
+//         })
+
+        
+//     });
+
+// // Get product ID with searchparams
+// function getProductId() {
+//     return new URL(window.location.href).searchParams.get('id');
 // }
 
-// });
+// // Get color value to the DOM
+// function getProductColor() {
+//     const colorValue = document.getElementById('colors');
+//     const colorOptions = colorValue.value;
+//     console.log(colorOptions);
+// }
+
+// // Get quantity value to the DOM
+// function getProductQuantity() {
+//     const quantity = document.getElementById('quantity');
+//     const quantityValue = quantity.value;
+//     console.log(quantityValue);
+// }
+
 
--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,5 @@
 const productId = getProductId();
 
-
 fetch('http://localhost:3000/api/products/' + productId)
     .then(data => data.json())
     .then(jsonProduct => {
@@ -12,9 +11,9 @@ fetch('http://localhost:3000/api/products/' + productId)
         
         // For selecting color 
         const colorsSelect = jsonProduct.colors;
+        
         for (const color of colorsSelect) {
-            document.getElementById('colors').innerHTML += `<option value="${color.toLowerCase()}">${color}</option>`;  
-            console.log(color);                                    
+            document.getElementById('colors').innerHTML += `<option value="${color.toLowerCase()}">${color}</option>`;                                     
         }
         
         // Collecting images from API
@@ -22,71 +21,77 @@ fetch('http://localhost:3000/api/products/' + productId)
         const productImageAlt = jsonProduct.altTxt;
         document.querySelector('.item__img').innerHTML = `<img src="${productImageUrl}" alt="${productImageAlt}">`;
  
-        const getProductQuantity = document.getElementById('quantity');
+        // Adding products to cart + eventListener action on click
+        const addToCart = document.getElementById("addToCart");
 
-        // //  Add product to cart
-        // const couleur = document.getElementById('colors').getElementsByTagName('option');
-        // const couleur = document.getElementById('colors');
-        // console.log(couleur);
+        addToCart.addEventListener('click', (event) => {
 
-        // const choixCouleur = couleur.innerText;
-        // console.log(choixCouleur);
-
-        let productArray = [
-            {
-                id: productId,
-                quantity : parseInt(getProductQuantity.value),
-                color: couleur
-            }
-        ];
-        console.log(productArray);
-
-        for (let i = 0; i < productArray.length; i ++) {
-        document.getElementById("addToCart").addEventListener('click', () => {
-            cartQuantity(productArray[i]);
-        });
-    }
-    
-        
-        function cartQuantity(product) {
-
-            let alreadyInCart = localStorage.getItem('cartQuantity');
+            // Get quantity value to the DOM
+            const getProductQuantity = document.getElementById('quantity');
             
-            alreadyInCart =parseInt(alreadyInCart);
+            // Get color value to the DOM
+            const colorValue = document.getElementById('colors');
+            const colorOptions = colorValue.value;
+
+            // Array creation for Local storage
+            const productArray = 
+                {
+                    name : jsonProduct.name,
+                    id : productId,
+                    quantity : parseInt(getProductQuantity.value),
+                    color : colorOptions
+                };
+                cartNumbers(productArray);
+                event.preventDefault();
             
-            if (alreadyInCart) {
-                localStorage.setItem('cartQuantity', alreadyInCart + parseInt(getProductQuantity.value));
-            } else {
-                localStorage.setItem('cartQuantity', parseInt(getProductQuantity.value));
+            // Create a function to add products in localstorage
+            function cartNumbers(product) {
+                console.log(product.color);
+                let productNumbers = localStorage.getItem('cartNumbers');
+                productNumbers = parseInt(productNumbers);
+                
+                if (productNumbers) {
+                    localStorage.setItem('cartNumbers', productNumbers + parseInt(getProductQuantity.value));
+                } else {
+                    localStorage.setItem('cartNumbers', parseInt(getProductQuantity.value));
+                }
+                setItems(product);
             }
-            setItems(product);
-        }
-        function setItems(product) {
-            let cartItems = localStorage.getItem('productsInCart');
-            cartItems = JSON.parse(cartItems);
-            console.log("My cart items are", cartItems);
-        
-            if(cartItems != null) {
-                if (cartItems[product.id] == undefined) {
-                    cartItems = {
-                        ...cartItems,
-                        [product.id]: product
+            function setItems(product) {
+                let alreadyInCart = localStorage.getItem('productsInCart');
+                alreadyInCart = JSON.parse(alreadyInCart);
+                console.log('I already added', alreadyInCart);
+                if (alreadyInCart != null) {
+                    if (alreadyInCart[product.name] == undefined) {
+                        console.log(alreadyInCart[product.name]);
+                        alreadyInCart = {
+                            ...alreadyInCart,
+                            [product.name]: product,
+                        }
+                        alreadyInCart[product.name].quantity;
+                    } else {
+                        alreadyInCart[product.name].quantity += product.quantity;
+                    }
+                    
+                } else {
+                    product.quantity;
+                    alreadyInCart = {
+                        [product.name]: product
                     }
                 }
-                cartItems[product.id].quantity += parseInt(getProductQuantity.value);
-            } else {
-                product.quantity = parseInt(getProductQuantity.value);
-                cartItems = {
-                    [product.id]: product
-                }
+
+                
+
+                
+                localStorage.setItem('productsInCart', JSON.stringify(alreadyInCart));
             }
-            
-            localStorage.setItem("productsInCart", JSON.stringify(cartItems));
-        }
+        })
     });
 
 function getProductId() {
     return new URL(window.location.href).searchParams.get('id');
 }
+
+
 
 
